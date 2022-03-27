bfccfe0eda633ae4e0d122d5c9b66c213e1b49d2
Riddhi Pawar <47048717+rid17pawar@users.noreply.github.com>
2022-03-27 15:19:22 +0530

    updated comments

--- backend/uploaded_file_to_resized_image.py
+++ backend/uploaded_file_to_resized_image.py
@@ -12,11 +12,11 @@ def convert_uploaded_file_to_image(data):
 
 def resize_image(image: Image.Image):
     image = image.resize([IMAGE_SIZE, IMAGE_SIZE])
-    shapeimg= np.shape(image)
+    #shapeimg= np.shape(image)
     
     image = np.asfarray(image)
     image = image/255.0
-    # predict() of model takes batch of images as input not a single image so, add 1 dimension
+    # predict() takes batch of images as input not a single image so, add 1 dimension
     img_batch = np.expand_dims(image, 0) 
-    shapeimgbatch= np.shape(img_batch)
+    #shapeimgbatch = np.shape(img_batch)
     return img_batch    
